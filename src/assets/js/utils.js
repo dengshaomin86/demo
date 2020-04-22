5bb282f24265044131d2078609f0b0b004667c82
dengshaomin86 <1101869752@qq.com>
2020-04-22 21:59:38 +0800

    init

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -2,3 +2,8 @@ import Vue from "vue";
 import directive from "./directive.js";
 
 Vue.use(directive);
+
+// 首字母大写换小写
+export function replaceFirst(str) {
+  return str.replace(/(|^)[A-Z]/g, (L) => L.toLowerCase());
+}
