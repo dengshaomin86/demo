ff7104d0f6ddae58da7021d3f4957f87e479b709
dengshaomin86 <1101869752@qq.com>
2020-04-25 16:03:04 +0800

    init

--- src/assets/js/axios.js
+++ src/assets/js/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8088";
+// axios.defaults.baseURL = "http://127.0.0.1:8088";
+axios.defaults.baseURL = "http://139.9.50.13:3000";
 
 window.axios = axios;
