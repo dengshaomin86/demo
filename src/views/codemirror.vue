b6b4166feeb1ac02ac0685a7b742197d43af0a22
dengshaomin86 <1101869752@qq.com>
2021-08-24 13:07:05 +0800

    tinymce codemirror

---
+++ src/views/codemirror.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>code mirror</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodeMirror',
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/tinymce.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>tinymce</h1>
+    <editor
+      api-key="no-api-key"
+      :init="{
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'code undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help',
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue';
+
+export default {
+  name: 'tinymce',
+  components: {
+    editor: Editor,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
