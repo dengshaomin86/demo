e58c7cfb3d9dbf529384705dac35a38917753868
dengshaomin86 <1101869752@qq.com>
2021-08-24 12:47:33 +0800

    init

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
+  trailingComma: 'all', // 尾随逗号 <es5|none|all>
   singleQuote: true, // 使用单引号
-  printWidth: 200, // 单行最大值
+  printWidth: 160, // 单行最大值
+  proseWrap: 'always', // 超过长度总是换行
   semi: true, // 结尾分号
-  disableLanguages: [] // 排除文件
+  disableLanguages: [], // 排除文件
 };
